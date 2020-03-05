9aa6e335178490a4cd27e0e621076f7937e4c65f
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2020-03-04 20:30:17 -0800

    Add structs and parser for cycling power data.

--- src/main.rs
+++ src/main.rs
@@ -226,8 +226,107 @@ fn parse_csc_measurement(data: Vec<u8>) -> CscMeasurement {
                     0,
                     0,
                 ]),
-                last_revolution_event_time: u16::from_le_bytes([data[crank_index + 2], data[crank_index + 3]])
-                    as f64
+                last_revolution_event_time: u16::from_le_bytes([
+                    data[crank_index + 2],
+                    data[crank_index + 3],
+                ]) as f64
+                    / 1024.0,
+            })
+        } else {
+            None
+        },
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum AccumulatedTorqueSource {
+    Wheel,
+    Crank,
+}
+
+// A Struct that does not care about bit compression
+#[derive(Debug, PartialEq)]
+pub struct CyclingPowerMeasurement {
+    instantaneous_power: i16,
+    // Notably this is _truly_ a percent, not a rate
+    // conversion to rate would be lossly
+    pedal_power_balance_percent: Option<f32>,
+    // Sum of the average torque measured per source rotation. Divide by
+    // rotations to get average torque or multiply by 2pi to get total energy.
+    // If you know the gearing you can translate from one source to the other.
+    // Divide energy by source time to get average power.
+    accumulated_torque: Option<(AccumulatedTorqueSource, f64)>,
+    wheel_revolution_data: Option<RevolutionData>,
+    crank_revolution_data: Option<RevolutionData>,
+    // TODO: There are other fields, but they're all after these or in the flags
+}
+
+// Notably, this function always assumes a valid input
+fn parse_cycling_power_measurement(data: Vec<u8>) -> CyclingPowerMeasurement {
+    let has_pedal_power_balance = data[0] & 1 == 1;
+    let has_accumulated_torque = data[0] & 0b100 == 0b100;
+    let has_wheel_data = data[0] & 0b10000 == 0b10000;
+    let has_crank_data = data[0] & 0b100000 == 0b100000;
+    let power_index = 2;
+    let pedal_power_balance_index = 4;
+    let accumulated_torque_index =
+        pedal_power_balance_index + if has_pedal_power_balance { 1 } else { 0 };
+    let wheel_data_index = accumulated_torque_index + if has_accumulated_torque { 2 } else { 0 };
+    let crank_data_index = wheel_data_index + if has_wheel_data { 6 } else { 0 };
+
+    CyclingPowerMeasurement {
+        instantaneous_power: i16::from_le_bytes([data[power_index], data[power_index + 1]]),
+        pedal_power_balance_percent: if has_pedal_power_balance {
+            Some(data[pedal_power_balance_index] as f32 / 2.0)
+        } else {
+            None
+        },
+        accumulated_torque: if has_accumulated_torque {
+            let source = if data[0] & 0b1000 == 0b1000 {
+                AccumulatedTorqueSource::Crank
+            } else {
+                AccumulatedTorqueSource::Wheel
+            };
+            let torque = u16::from_le_bytes([
+                data[accumulated_torque_index],
+                data[accumulated_torque_index + 1],
+            ]) as f64
+                / 32.0;
+            Some((source, torque))
+        } else {
+            None
+        },
+        // This isn't quite identical to CSC wheel data: it's /2048 instead of /1024
+        wheel_revolution_data: if has_wheel_data {
+            Some(RevolutionData {
+                revolution_count: u32::from_le_bytes([
+                    data[wheel_data_index],
+                    data[wheel_data_index + 1],
+                    data[wheel_data_index + 2],
+                    data[wheel_data_index + 3],
+                ]),
+                last_revolution_event_time: (u16::from_le_bytes([
+                    data[wheel_data_index + 4],
+                    data[wheel_data_index + 4 + 1],
+                ]) as f64)
+                    / 2048.0,
+            })
+        } else {
+            None
+        },
+        // This is identical to CSC crank data
+        crank_revolution_data: if has_crank_data {
+            Some(RevolutionData {
+                revolution_count: u32::from_le_bytes([
+                    data[crank_data_index],
+                    data[crank_data_index + 1],
+                    0,
+                    0,
+                ]),
+                last_revolution_event_time: u16::from_le_bytes([
+                    data[crank_data_index + 2],
+                    data[crank_data_index + 2 + 1],
+                ]) as f64
                     / 1024.0,
             })
         } else {
