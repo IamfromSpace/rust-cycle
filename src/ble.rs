5470bb411d03e071e363f7cabfdb7de64372dc71
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2020-03-22 09:58:24 -0700

    Move HeartRateMeasurement out into its own module.

---
+++ src/ble.rs
@@ -0,0 +1,1 @@
+pub mod heart_rate_measurement;
---
+++ src/ble/heart_rate_measurement.rs
@@ -0,0 +1,214 @@
+// A Struct that does not care about bit compression
+#[derive(Debug, PartialEq, Clone)]
+pub struct HeartRateMeasurement {
+    // since this is not in SI, its units are defined in its name.
+    pub bpm: u16,
+    // If sensor contact is not supported, this is None, otherwise the boolean
+    // value will tell you.
+    pub is_sensor_contact_detected: Option<bool>,
+    // Note that this _could_ overflow for very very long rides, but that makes
+    // an otherwise snapshot-only measurement need prior context.  This is in
+    // Joules.
+    pub energy_expended: Option<u16>,
+    // This is list of the time (in seconds) measured between R-Wave detections.
+    // It is an array, because there may be many intervals recorded during a
+    // single notification window (or there may be none).  Measurements are
+    // indexed by time, so the 0-index reading is the oldest. A 32-bit float is
+    // a lossless representation of the original data sent by the device.  Note
+    // that (at least on Polar H10 devices) when the frequency of beats is lower
+    // than the frequency of notifications, there's no way to distinguish
+    // between zero detections and this feature not being supported on the
+    // device, which is why this is not an Option.
+    pub rr_intervals: Vec<f32>,
+}
+
+// Notably, this function always assumes a valid input
+pub fn parse_hrm(data: &Vec<u8>) -> HeartRateMeasurement {
+    let is_16_bit = data[0] & 1 == 1;
+    let has_sensor_detection = data[0] & 0b100 == 0b100;
+    let has_energy_expended = data[0] & 0b1000 == 0b1000;
+    let energy_expended_index = 2 + if is_16_bit { 1 } else { 0 };
+    let rr_interval_index =
+        2 + if has_energy_expended { 2 } else { 0 } + if is_16_bit { 1 } else { 0 };
+    HeartRateMeasurement {
+        bpm: if is_16_bit {
+            u16::from_le_bytes([data[1], data[2]])
+        } else {
+            data[1] as u16
+        },
+        is_sensor_contact_detected: if has_sensor_detection {
+            Some(data[0] & 0b10 == 0b10)
+        } else {
+            None
+        },
+        energy_expended: if has_energy_expended {
+            Some(u16::from_le_bytes([
+                data[energy_expended_index],
+                data[energy_expended_index + 1],
+            ]))
+        } else {
+            None
+        },
+        rr_intervals: {
+            let rr_interval_count = (data.len() - rr_interval_index) / 2;
+            let mut vec = Vec::with_capacity(rr_interval_count);
+            for i in 0..rr_interval_count {
+                let as_u16 = u16::from_le_bytes([
+                    data[rr_interval_index + 2 * i],
+                    data[rr_interval_index + 2 * i + 1],
+                ]);
+                vec.push(as_u16 as f32 / 1024.0);
+            }
+            vec
+        },
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_hrm;
+    use super::HeartRateMeasurement;
+
+    #[test]
+    fn parse_hrm_16_bit_energy_expended_and_one_rr_intervals() {
+        assert_eq!(
+            HeartRateMeasurement {
+                bpm: 70,
+                is_sensor_contact_detected: None,
+                energy_expended: Some(523),
+                rr_intervals: vec!(266.0 / 1024.0)
+            },
+            parse_hrm(&vec!(0b11001, 70, 0, 11, 2, 10, 1))
+        );
+    }
+
+    #[test]
+    fn parse_hrm_16_bit_and_one_rr_intervals() {
+        assert_eq!(
+            HeartRateMeasurement {
+                bpm: 70,
+                is_sensor_contact_detected: None,
+                energy_expended: None,
+                rr_intervals: vec!(266.0 / 1024.0)
+            },
+            parse_hrm(&vec!(0b10001, 70, 0, 10, 1))
+        );
+    }
+
+    #[test]
+    fn parse_hrm_and_three_rr_intervals() {
+        assert_eq!(
+            HeartRateMeasurement {
+                bpm: 70,
+                is_sensor_contact_detected: None,
+                energy_expended: None,
+                rr_intervals: vec!(266.0 / 1024.0, 523.0 / 1024.0, 780.0 / 1024.0)
+            },
+            parse_hrm(&vec!(0b10000, 70, 10, 1, 11, 2, 12, 3))
+        );
+    }
+
+    #[test]
+    fn parse_hrm_and_one_rr_intervals() {
+        assert_eq!(
+            HeartRateMeasurement {
+                bpm: 70,
+                is_sensor_contact_detected: None,
+                energy_expended: None,
+                rr_intervals: vec!(266.0 / 1024.0)
+            },
+            parse_hrm(&vec!(0b10000, 70, 10, 1))
+        );
+    }
+
+    #[test]
+    fn parse_hrm_16_bit_and_energy_expended() {
+        assert_eq!(
+            HeartRateMeasurement {
+                bpm: 70,
+                is_sensor_contact_detected: None,
+                energy_expended: Some(266),
+                rr_intervals: Vec::with_capacity(0),
+            },
+            parse_hrm(&vec!(0b1001, 70, 0, 10, 1))
+        );
+    }
+
+    #[test]
+    fn parse_hrm_and_energy_expended() {
+        assert_eq!(
+            HeartRateMeasurement {
+                bpm: 70,
+                is_sensor_contact_detected: None,
+                energy_expended: Some(266),
+                rr_intervals: Vec::with_capacity(0),
+            },
+            parse_hrm(&vec!(0b1000, 70, 10, 1))
+        );
+    }
+
+    #[test]
+    fn parse_hrm_without_contact() {
+        assert_eq!(
+            HeartRateMeasurement {
+                bpm: 70,
+                is_sensor_contact_detected: Some(false),
+                energy_expended: None,
+                rr_intervals: Vec::with_capacity(0),
+            },
+            parse_hrm(&vec!(0b100, 70))
+        );
+    }
+
+    #[test]
+    fn parse_hrm_with_contact() {
+        assert_eq!(
+            HeartRateMeasurement {
+                bpm: 70,
+                is_sensor_contact_detected: Some(true),
+                energy_expended: None,
+                rr_intervals: Vec::with_capacity(0),
+            },
+            parse_hrm(&vec!(0b110, 70))
+        );
+    }
+
+    #[test]
+    fn parse_hrm_16_bit_big_simple() {
+        assert_eq!(
+            HeartRateMeasurement {
+                bpm: 266,
+                is_sensor_contact_detected: None,
+                energy_expended: None,
+                rr_intervals: Vec::with_capacity(0),
+            },
+            parse_hrm(&vec!(1, 10, 1))
+        );
+    }
+
+    #[test]
+    fn parse_hrm_16_bit_simple() {
+        assert_eq!(
+            HeartRateMeasurement {
+                bpm: 70,
+                is_sensor_contact_detected: None,
+                energy_expended: None,
+                rr_intervals: Vec::with_capacity(0),
+            },
+            parse_hrm(&vec!(1, 70, 0))
+        );
+    }
+
+    #[test]
+    fn parse_hrm_simplest() {
+        assert_eq!(
+            HeartRateMeasurement {
+                bpm: 70,
+                is_sensor_contact_detected: None,
+                energy_expended: None,
+                rr_intervals: Vec::with_capacity(0),
+            },
+            parse_hrm(&vec!(0, 70))
+        );
+    }
+}
