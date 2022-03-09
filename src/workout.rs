4eff3026e0657bdb518b811bd7e45cbba02edabd
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2022-03-08 21:25:09 -0800

    Add a workout offset that can be changed via the WorkoutHandle.

--- src/workout.rs
+++ src/workout.rs
@@ -13,20 +13,35 @@ pub struct Workout {
     tail: Option<u16>,
 }
 
+#[derive(Clone)]
+pub struct WorkoutState {
+    running: bool,
+    offset: i16,
+}
+
 pub struct WorkoutHandle {
-    running: Arc<Mutex<bool>>,
+    pub state: Arc<Mutex<WorkoutState>>,
     join_handle: Option<JoinHandle<()>>,
 }
 
 impl WorkoutHandle {
     pub fn exit(&mut self) {
         {
-            let mut running = self.running.lock().unwrap();
-            *running = false;
+            let mut state = self.state.lock().unwrap();
+            state.running = false;
         }
         if let Some(jh) = mem::replace(&mut self.join_handle, None) {
             jh.join().unwrap();
         }
+    }
+}
+
+// TODO: This helper is only here because it's clunky to access the state,
+// because WorkoutHandle can't be clone (because of JoinHandle).
+pub fn add_offset(state: &Arc<Mutex<WorkoutState>>, offset: i16) {
+    {
+        let mut state = state.lock().unwrap();
+        state.offset += offset;
     }
 }
 
@@ -42,27 +57,41 @@ impl Workout {
     // correct total time for our changes.
     pub fn run<F: Fn(u16) + Send + 'static>(self, start: Instant, set_power: F) -> WorkoutHandle {
         // TODO: There must be a more elegant way to do this
-        let running = Arc::new(Mutex::new(true));
-        let running_for_thread = running.clone();
+        let state = Arc::new(Mutex::new(WorkoutState {
+            running: true,
+            offset: 0,
+        }));
+        let state_for_thread = state.clone();
         let Workout { ct, tail } = self;
         let tail_iter = tail.map(|x| (Duration::from_secs(1000000), x)).into_iter();
         let join_handle = Some(thread::spawn(move || {
             let mut d = Duration::from_secs(0);
+            let mut last_offset: i16 = 0;
+
             for (wait, power) in ct.into_iter().chain(tail_iter) {
                 // Overflow is not a consideration for the timeline of a single workout
                 d = d.checked_add(wait).unwrap();
                 let e = start.elapsed();
                 // If duration is negative, we continue on.
                 if let Some(_) = d.checked_sub(e) {
-                    set_power(power);
+                    set_power(((power as i16) + last_offset) as u16);
 
                     // We loop and check every 50ms if we should move to the
                     // next power or if the workout is teriminated.
                     let terminate = loop {
                         thread::sleep(Duration::from_millis(50));
                         {
-                            if !*running_for_thread.lock().unwrap() {
+                            let state = state_for_thread.lock().unwrap();
+                            if !state.running {
                                 break true;
+                            }
+
+                            // Check to see if the offset has changed, if so,
+                            // record the new offset and immediately update the
+                            // power.
+                            if state.offset != last_offset {
+                                last_offset = state.offset;
+                                set_power(((power as i16) + last_offset) as u16);
                             }
                         }
                         if let None = d.checked_sub(start.elapsed()) {
@@ -76,10 +105,7 @@ impl Workout {
             }
         }));
 
-        WorkoutHandle {
-            join_handle,
-            running,
-        }
+        WorkoutHandle { join_handle, state }
     }
 }
 
